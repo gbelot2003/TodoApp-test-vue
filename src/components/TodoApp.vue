d419cb23cbd4719d29ac07726e738fda4d62c50c
Gerardo A Belot <gbelot2003@hotmail.com>
2022-03-25 01:08:48 -0600

    🏃 ON: creating the TodoApp component

---
+++ src/components/TodoApp.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'TodoApp',
+  data(){
+    return {
+      todos: [
+        {
+          id: 1,
+          text: "Lear vue testing",
+          completed: false,
+        }
+      ]
+    }
+  }
+}
+</script>

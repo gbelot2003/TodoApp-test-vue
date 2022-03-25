e5df90a992f911a3d6e13059be1d3211fa239592
Gerardo A Belot <gbelot2003@hotmail.com>
2022-03-25 01:44:23 -0600

    🏃 ON: creating new-todo form and add to the list

--- src/components/TodoApp.vue
+++ src/components/TodoApp.vue
@@ -1,21 +1,38 @@
 <template>
   <div>
+    <div v-for="todo in todos" :key="todo.id" data-test="todo">
+      {{ todo.text }}
+    </div>
+    <form data-test="form" @submit.prevent="createTodo">
+      <input v-model="newTodo" data-test="new-todo" />
+    </form>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'TodoApp',
-  data(){
+  name: "TodoApp",
+  data() {
     return {
+      newTodo: "",
       todos: [
         {
           id: 1,
-          text: "Lear vue testing",
+          text: "Learn Vue Test",
           completed: false,
-        }
-      ]
-    }
-  }
-}
+        },
+      ],
+    };
+  },
+  methods: {
+    createTodo() {
+      this.todos.push({
+        id: 2,
+        text: this.newTodo,
+        completed: false,
+      });
+      this.newTodo = ""
+    },
+  },
+};
 </script>
---
+++ tests/unit/TodoApp.spec.js
@@ -0,0 +1,25 @@
+import { mount } from "@vue/test-utils";
+import TodoApp from "@/components/TodoApp";
+
+describe("TodoApp", () => {
+  it("should render todo text", () => {
+    const wrapper = mount(TodoApp);
+
+    const todo = wrapper.get('[data-test="todo"]');
+
+    expect(todo.text()).toBe("Learn Vue Test");
+  });
+
+  it("should add new todo", async () => {
+    const wrapper = mount(TodoApp);
+
+    expect(wrapper.findAll('[data-test="todo"]')).toHaveLength(1);
+
+    await wrapper.get('[data-test="new-todo"]').setValue("New Todo");
+    await wrapper.get('[data-test="form"]').trigger("submit");
+
+    expect(wrapper.findAll('[data-test="todo"]')).toHaveLength(2);
+
+    expect(wrapper.find('[new-todo=""]'));
+  });
+});
